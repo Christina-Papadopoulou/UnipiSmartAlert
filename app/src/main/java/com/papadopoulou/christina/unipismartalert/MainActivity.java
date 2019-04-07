62901ed1a318b5bcb7925c74f70ef6f59d62c7f0
dev-christina <christina.pap1988@gmail.com>
2019-04-07 16:36:41 +0300

    Detect falling test

--- app/src/main/java/com/papadopoulou/christina/unipismartalert/MainActivity.java
+++ app/src/main/java/com/papadopoulou/christina/unipismartalert/MainActivity.java
@@ -6,6 +6,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -30,9 +31,32 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onResume();
         sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
-
+    double prevTime = 0;
+    int  tempX = 0;
+    int  tempY = 0;
+    int  tempZ = 0;
     @Override
     public void onSensorChanged(SensorEvent event) {
+        double curTime = System.currentTimeMillis();
+
+
+            prevTime = curTime;
+            int x = Math.round(event.values[0]);
+            int y = Math.round(event.values[1]);
+            int z = Math.round(event.values[2]);
+
+
+            if(x == tempX & y == tempY & z != tempZ){
+                Log.e("XRISTINA", "PTOSI");
+            }
+
+            tempX = x;
+            tempY = y;
+            tempZ = z;
+
+
+
+            Log.e("XRISTINA", "X " + x + "  Y " + y + "  Z " + z);
 
     }
 
