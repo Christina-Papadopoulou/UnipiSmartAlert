bdc2679f6abc26bc2187a487b43842d036186691
dev-christina <christina.pap1988@gmail.com>
2019-04-15 21:30:26 +0300

    Problem Solved. Need Test From Christina

--- app/src/main/java/com/papadopoulou/christina/unipismartalert/LoginActivity.java
+++ app/src/main/java/com/papadopoulou/christina/unipismartalert/LoginActivity.java
@@ -3,7 +3,9 @@ package com.papadopoulou.christina.unipismartalert;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -68,8 +70,17 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(),SingUpActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent,200);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 200){
+            Log.e("JIM", "Done " + requestCode);
+        }
     }
 }
--- app/src/main/java/com/papadopoulou/christina/unipismartalert/SingUpActivity.java
+++ app/src/main/java/com/papadopoulou/christina/unipismartalert/SingUpActivity.java
@@ -1,5 +1,6 @@
 package com.papadopoulou.christina.unipismartalert;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -53,6 +54,8 @@ public class SingUpActivity extends AppCompatActivity {
                                 myRef.setValue(usersMap);
 
                                 //TODO call back to Main Activity
+                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                startActivityForResult(intent, 200);
                             }
                         }
                     }
